1be81a1e7c265093ecffcbdbd7ebf69110bafb3c
Vincent Augugliaro <vincent.augugliaro@orange.fr>
2021-02-24 18:38:20 +0100

    add features to the cart

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,8 @@
+import * as Cart from './modules/cart.js';
+
+/* mise à jour des quantités dans le panier */
+Cart.refresh();
+
 /* Envoi de la demande à l'API : "tous les teddies" */
 fetch("http://localhost:3000/api/teddies")
 .then(response => response.json())
@@ -12,12 +17,12 @@ function displayTeddies(datas) {
         productsContent += 
             '<div class="col">' +
             '<div class="card mb-4 mb-lg-0 h-100 bg-light border-secondary shadow-sm">' +
-            '<img src="'+ data.imageUrl +'" alt="'+ data.name +' l\'ours en peluche" class="card-img-top">' +
+            '<img src="'+ data.imageUrl +'" alt="'+ data.name +' l\'ours en peluche" class="card-img-top h-75">' +
             '<div class="card-body">' +
             '<h2 class="card-title h5">'+ data.name +'</h2>' +
             '<p class="card-text">Cet adorable petit ours en peluche n\'attend que toi.</p>' +
             '</div>' +
-            '<div class="card-footer ">' +
+            '<div class="card-footer text-center">' +
             '<a class="btn btn-primary text-white fw-bold stretched-link" href="./ours-en-peluche.html?teddy='+ data._id +'" role="button">Adopte-moi</a>' +
             '</div>' +
             '</div>' +
@@ -25,3 +30,5 @@ function displayTeddies(datas) {
     }
     document.getElementById("products").innerHTML = productsContent;  
 }
+
+//Cart.reset(); //POUR TEST A SUPPRIMER
